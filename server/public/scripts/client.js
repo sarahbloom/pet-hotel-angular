cb447f0f2cf2290f2d28f2462a64fea0efbcfdaf
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-04-05 11:05:51 -0500

    html views working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,14 @@ let PetHotelApp = angular.module('PetHotelApp', ['ngRoute', 'ngMaterial']);
 PetHotelApp.config(['$routeProvider', function ($routeProvider) {
     console.log('Route config loaded');
 
+    $routeProvider
+    .when('/owner',{
+        templateUrl: '/views/owner.html',
+        controller: 'OwnerController as vm'
+    }).when('/pet', {
+        templateUrl: '/views/pet.html',
+        controller: 'PetController as vm'
+    }).otherwise(
+        { redirectTo: '/pet' }
+    )
 }]);
