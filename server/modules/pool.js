b560146297c0a4b0a8e5ec211f004d05154c6311
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-04-09 08:50:13 -0500

    added Heroku route to DB

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,8 +1,10 @@
-// PG CONFIG SETUP
 const pg = require('pg');
-const Pool = pg.Pool;
+const url = require('url');
+let config = {};
 
 if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
     let params = url.parse(process.env.DATABASE_URL);
     let auth = params.auth.split(':');
 
@@ -13,35 +15,20 @@ if (process.env.DATABASE_URL) {
         port: params.port,
         database: params.pathname.split('/')[1],
         ssl: true, // heroku requires ssl to be true
-        max: 10, // max number of clients in the pool
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+        max: 10, 
+        idleTimeoutMillis: 30000, 
     };
 
 } else {
- 
     config = {
-        user: process.env.PG_USER || null, 
-        password: process.env.DATABASE_SECRET || null,
+        user: process.env.PG_USER || null,
+        password: process.env.DATABASE_SECRET || null, 
         host: process.env.DATABASE_SERVER || 'localhost', 
         port: process.env.DATABASE_PORT || 5432, 
         database: process.env.DATABASE_NAME || 'petHotel',
-        max: 10, 
+        max: 10,
         idleTimeoutMillis: 30000, 
     };
 }
 
-// create a new pool instance to manage connections
-const pool = new Pool(config);
-
-pool.on('connect', (client) => {
-    console.log('Postgesql connected');
-})
-
-pool.on('error', (err, client) => {
-    console.log('Unexpected error on idle client', err);
-    process.exit(-1);
-});
-
-module.exports = pool;
-
-
+module.exports = new pg.Pool(config);
